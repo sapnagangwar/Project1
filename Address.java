8e7cb578b53315593712c70201c6a938147dc372
sapnagangwar <sapna.iaf@gmail.com>
2016-02-26 13:48:06 -0800

    Address Class

---
+++ Address.java
@@ -0,0 +1,16 @@
+package hw_project;
+
+/**
+ * One object of this class represents one address,including street address, city, state and zip.
+ */
+class Address {
+private String streetAddress;
+private String city;
+private String state;
+private String zip;
+
+public Address(){};
+public void read(){};
+public String toString(){};
+}
+
