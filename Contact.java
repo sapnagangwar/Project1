080adc646ead7a485266684b6014d1a3ebe60d87
sapnagangwar <sapna.iaf@gmail.com>
2016-02-26 13:50:20 -0800

    Contact Class

---
+++ Contact.java
@@ -0,0 +1,21 @@
+package hw_project;
+
+/**
+ * One object of this class represents info about one Contact,including name, address, 
+ * email address, phone number and notes .
+ */
+
+class Contact {
+
+private Name name;
+private Address address;
+private String emailAddress;
+private String phoneNumber;
+private String notes;
+
+
+public Contact(){};
+public void read(){};
+public String toString(){};
+}
+
