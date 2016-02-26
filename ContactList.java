d7f54037c78549df89d213009492b73f0b06d766
sapnagangwar <sapna.iaf@gmail.com>
2016-02-26 13:37:03 -0800

    Added files via upload

---
+++ ContactList.java
@@ -0,0 +1,26 @@
+/**
+ * one object of class ContactList represents a whole list of contacts.
+ */
+package hw_project;
+import java.util.*;
+
+public class ContactList {
+     private ArrayList<Contact> myContactList = new ArrayList<Contact>();
+
+    /**
+     * Constructor that allocates space for the ArrayList.
+     */
+    public ContactList() {}
+    
+    /**
+     * Creates a new Contact object and adds it to the ArrayList.
+     */
+    public void addNewContact()  {}
+
+    /**
+     * Returns a String containing all the data in the ArrayList.
+     */
+
+    public String toString()  {}
+
+}
